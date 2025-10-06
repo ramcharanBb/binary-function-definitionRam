1edffb591f78f8a3cf53d734786c8c768d14b237
harshini-v-m <harshini.blubridge@evoplus.in>
2025-10-06 12:35:16 +0530

    Update NovaOps.cpp
    added required functions for multiply and division op

--- lib/Dialect/nova/NovaOps.cpp
+++ lib/Dialect/nova/NovaOps.cpp
@@ -9,8 +9,7 @@ using namespace mlir::nova;
 #define GET_OP_CLASSES
 #include "Compiler/Dialect/nova/NovaOps.cpp.inc"
 
-
-//----------------------add op functions ---------------------
+//----------------------ADDITION op functions ---------------------
 // 1 . inferReturnTypes
 // 2 . verify
 
@@ -58,10 +57,10 @@ LogicalResult AddOp::verify() {
   return success();
 }
 
-//----------------------sub op functions ---------------------
+//----------------------SUBTRACT op functions ---------------------
 // 1 . inferReturnTypes
 
- /*constraints on input : 
+/*constraints on input : 
   1 -> 2 operands 
   2 -> They need to be same or compatible type 
   3 -> compatible means they can be made same by broadcasting
@@ -70,6 +69,7 @@ LogicalResult AddOp::verify() {
   constraint on output:
   1-> result and operands type should be same
   2 -> if broadcaseted result type needs to be same as operands type after broadcasting*/
+
 
 LogicalResult SubOp::inferReturnTypes(
     MLIRContext *context,
@@ -88,4 +88,75 @@ LogicalResult SubOp::inferReturnTypes(
   }
   return failure();
 }
- 
+
+
+
+//---------------------- element wise MULTIPLY op functions ---------------------
+// 1 . inferReturnTypes
+
+/*constraints on input : 
+  1 -> 2 operands 
+  2 -> The dimension and rank need to be same or compatible type 
+  3 -> compatible means they can be made same by broadcasting
+  4 -> should supported data types : int,float
+  5 -> the datatyes only need to be compatible
+
+  constraint on output:
+  1 -> result and operands type should be same
+  2 -> if broadcaseted result type needs to be same as operands type after broadcasting*/
+
+
+LogicalResult MulOp::inferReturnTypes(
+    MLIRContext *context,
+    std::optional<Location> loc,
+    ValueRange operands,
+    DictionaryAttr attributes,
+    OpaqueProperties properties,
+    RegionRange regions,
+    llvm::SmallVectorImpl<Type> &inferredReturnTypes) {
+
+  //create a function specific for this operation and call it here
+
+  if(operands[0].getType() == operands[1].getType()){
+    inferredReturnTypes.push_back(operands[0].getType());
+    return success();
+  }
+  return failure();
+}
+
+//---------------------- element wise DIVIDE op functions ---------------------
+// 1 . inferReturnTypes
+/*constraints on input : 
+  1 -> 2 operands 
+  2 -> The dimension and rank need to be same or compatible type 
+  3 -> compatible means they can be made same by broadcasting
+  4 -> should supported data types : int,float
+  5 -> the datatyes only need to be compatible
+
+  constraint on output:
+  1 -> result and operands type should be same
+  2 -> if broadcaseted result type needs to be same as operands type after broadcasting*/
+
+LogicalResult DivOp::inferReturnTypes(
+    MLIRContext *context,
+    std::optional<Location> loc,
+    ValueRange operands,
+    DictionaryAttr attributes,
+    OpaqueProperties properties,
+    RegionRange regions,
+    llvm::SmallVectorImpl<Type> &inferredReturnTypes) {
+
+  //create a function specific for this operation and call it here
+
+  if(operands[0].getType() == operands[1].getType()){
+    inferredReturnTypes.push_back(operands[0].getType());
+    return success();
+  }
+ // mlir::emitOptionalError(*loc,"Incompatible type");
+  return failure();
+}  
+
+
+
+
+
